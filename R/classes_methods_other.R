ab8b867c130bda766133078d5f587789ec939150
matbmeijer <matthias@brennotten.net>
2019-07-17 00:02:56 +0200

    First commit!

---
+++ R/classes_methods_other.R
@@ -0,0 +1,241 @@
+#' @title Default HTTPS status error messages
+#' @description Function which returns predefined default success/error messages as documented at \href{https://holidayapi.com/docs}{}.
+#' @param status HTTPS returned status code when doing a GET REST API call to any https://holidayapi.com endpoint.
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @return Returns a \code{character} with a predefined success/error linked to the status code.
+#' @source For more information about the Holiday API go to the \href{https://holidayapi.com/docs}{Holiday API documentation page}
+#' @examples
+#' \dontrun{
+#' api_response(200)
+#' }
+
+api_response<-function(status){
+  if(status==200){
+    response<-"Success! Everything is A-OK"
+  } else if(status==400){
+    response<-"Something is wrong on your end"
+  } else if(status==401){
+    response<-"Unauthorized (did you remember your API key?)"
+  } else if(status==402){
+    response<-"Payment required (account is delinquent)"
+  } else if(status==403){
+    response<-"Forbidden (this API is HTTPS-only)"
+  } else if(status==429){
+    response<-"Rate limit exceeded"
+  }else if(status==500){
+    response<-"OH NOES!!~! Something is wrong on our end"
+  }
+  return(response)
+}
+
+#' @title Add Country ISO Code column
+#' @description Function to add country 2 letter isocode column in subidivision \code{data.frame} item
+#' @param item country subdivision \code{data.frame} element
+#' @param country_code 2 letter iso code to assign
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @return Returns the inserted \code{data.frame} with the isocode as column attached.
+
+add_iso_code<-function(item, country_code){
+  if(is.null(item)){
+    item<-data.frame(code=NA, name=NA, languages=NA, country_code=country_code, stringsAsFactors = FALSE)
+  } else {
+    item$country_code<-country_code
+  }
+  return(item)
+}
+
+#' @title Converts non-usable values to empty character string
+#' @description Converts FALSE, character(0), NA, list(), NULL to an empty  \code{""} \code{character} string
+#' @param item Logical FALSE, empty, NULL or NA element
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @return Returns an empty \code{character} string.
+
+nonvalues2NA<-function(item){
+  if(is.na(item)||length(item)==0||is.null(item)||(is.logical(item)&&!item)){
+    item<-""
+  }
+  return(item)
+}
+
+#' @title Returns API default version
+#' @description Allows to easily adjust the versioning within the package
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @return Returns the package API version
+
+default_version <- function(){"v1"}
+
+#' @title Convert to \code{country_holidays} class
+#' @description Convert call response to an internal \code{country_holidays} class object, formatting the response to ease tha holidays analysis.
+#' @param request_url URL with which the RESTful GET API call has ben made.
+#' @param key The users API key obtained when registering at \href{https://holidayapi.com/signup}{}.
+#' @param country Country code in \href{https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2}{ISO 3166-1 alpha 2}
+#' or \href{https://en.wikipedia.org/wiki/ISO_3166-2}{ISO 3166-2} format. For multiple countries,
+#' pass a vector - it is limited to 10 countries. It is a required parameter in \code{character} format.
+#' @param year Year in \href{https://en.wikipedia.org/wiki/ISO_8601#Years}{ISO 8601} format.
+#' It is a required parameter in \code{numeric} format.
+#' @param call JSON Response from the RESTful GET API call formatted back to a \code{list} element.
+#' @param ... Optional parameters.
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @return Returns a \code{country_holidays} object class
+
+as_country_holidays<-function(request_url, key, country, year, call, ...){
+  arguments<-list( ...)
+  if(is.null(arguments$language)){
+    arguments$language="eng"
+  }
+  call$holidays$date<-as.Date(call$holidays$date)
+  call$holidays$observed<-as.Date(call$holidays$observed)
+
+  obj<-structure(
+    list(
+      status=call$status,
+      response=api_response(call$status),
+      key=key,
+      country=country,
+      year=year,
+      month=arguments$month,
+      day=arguments$day,
+      previous=arguments$previous,
+      upcoming=arguments$upcoming,
+      public=arguments$public,
+      language=arguments$language,
+      request_url = request_url,
+      observations = nrow(call$holidays),
+      holidays = call$holidays,
+      requests_used=call$requests$used,
+      requests_reset_date=as.Date(call$requests$resets),
+      requests_date=arguments$requests_date),
+    class =c("country_holidays", "list")
+    )
+  return(obj)
+}
+
+#' @title Convert to \code{available_countries} class
+#' @description Convert call response to an internal \code{available_countries} class object, formatting the response to ease tha holidays analysis.
+#' @param request_url URL with which the RESTful GET API call has ben made.
+#' @param key The users API key obtained when registering at \href{https://holidayapi.com/signup}{}.
+#' @param call JSON Response from the RESTful GET API call formatted back to a \code{list} element.
+#' @param ... Optional parameters.
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @return Returns a \code{available_countries} object class
+
+as_available_countries<-function(request_url, key, call, ...){
+  arguments<-list( ...)
+  call$countries$subdivisions<-mapply(add_iso_code,
+                                      item=call$countries$subdivisions,
+                                      country_code=call$countries$code,
+                                      SIMPLIFY = FALSE)
+  subdivisions<-do.call("rbind", call$countries$subdivisions)
+  countries<-call$countries
+  countries$subdivisions<-NULL
+  countries$languages<-lapply(countries$languages, nonvalues2NA)
+  subdivisions$languages<-lapply(subdivisions$languages, nonvalues2NA)
+  countries$languages<-unlist(lapply(countries$languages, paste0, collapse=", "), FALSE, FALSE)
+  subdivisions$languages<-unlist(lapply(subdivisions$languages, paste0, collapse=", "), FALSE, FALSE)
+  countries$languages[countries$languages==""]<-NA
+  subdivisions$languages[subdivisions$languages==""]<-NA
+  call$countries<-merge(countries,
+                        subdivisions,
+                        by.x ="code",
+                        by.y = "country_code",
+                        suffixes = c("_countries", "_subdivisions"),
+                        all.x = TRUE)
+
+  obj<-structure(
+    list(
+      status=call$status,
+      response=api_response(call$status),
+      key=key,
+      request_url = request_url,
+      observations = nrow(call$countries),
+      countries = call$countries,
+      requests_used=call$requests$used,
+      requests_reset_date=as.Date(call$requests$resets),
+      requests_date=arguments$requests_date),
+    class =c("available_countries", "list")
+  )
+  return(obj)
+}
+
+#' @title Convert to \code{available_languages} class
+#' @description Convert call response to an internal \code{available_languages} class object, formatting the response to ease tha holidays analysis.
+#' @param request_url URL with which the RESTful GET API call has ben made.
+#' @param key The users API key obtained when registering at \href{https://holidayapi.com/signup}{}.
+#' @param call JSON Response from the RESTful GET API call formatted back to a \code{list} element.
+#' @param ... Optional parameters.
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @return Returns a \code{available_languages} object class
+
+as_available_languages<-function(request_url, key, call, ...){
+  arguments<-list( ...)
+  obj<-structure(
+    list(
+      status=call$status,
+      response=api_response(call$status),
+      key=key,
+      request_url = request_url,
+      observations = nrow(call$languages),
+      languages = call$languages,
+      requests_used=call$requests$used,
+      requests_reset_date=as.Date(call$requests$resets),
+      requests_date=arguments$requests_date),
+    class =c("available_languages", "list")
+  )
+  return(obj)
+}
+
+
+
+print <- function(x){
+  UseMethod("print",x)
+}
+
+print.country_holidays <- function(x){
+  cat("<Holiday API - Holidays - ", x$request_url, ">\n", sep = "")
+  print(x$holidays)
+  invisible(x)
+}
+print.available_countries <- function(x){
+  cat("<Holiday API - Countries - ", x$request_url, ">\n", sep = "")
+  print(x$countries)
+  invisible(x)
+}
+print.available_languages <- function(x){
+  cat("<Holiday API - Languages - ", x$request_url, ">\n", sep = "")
+  print(x$languages)
+  invisible(x)
+}
+
+
+summary <- function(x){
+  UseMethod("summary",x)
+}
+
+
+summary.country_holidays <- function(x){
+  res<-sprintf("At date %s, %s holidays appear in %s during the year %s, from which %s are public.",
+               x$requests_date,
+               x$observations,
+               paste(x$country, collapse = " & "),
+               x$year,
+               nrow(x$holidays[x$holidays$public==TRUE,])
+  )
+  return(res)
+}
+summary.available_countries <- function(x){
+  res<-sprintf("At date %s, the Holiday API supports holiday information for %s countries and %s subdivisions.",
+               x$requests_date,
+               length(unique(x$countries$code)),
+               length(unique(x$countries$code_subdivisions))
+  )
+  return(res)
+}
+summary.available_languages <- function(x){
+  res<-sprintf("At date %s, the Holiday API supports %s language formats.",
+               x$requests_date,
+               length(unique(x$languages$name))
+  )
+  return(res)
+}
+
+
---
+++ R/get_holidays.R
@@ -0,0 +1,201 @@
+#' @title Saves the key for future functions
+#' @description Saves the key in the users environment as HOLIDAYAPI_PAT. It has the advantage that it is not necessary to explicitly publish the key in the users code. Just do it ones and you're set. To update the key kust save again and it will overwrite the old key.
+#' @param key The users API key obtained when registering at \href{https://holidayapi.com/signup}{}.
+#' @return Saves the key in the users environment as HOLIDAYAPI_PAT - it does not return any object.
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @examples
+#' \dontrun{
+#' gsave_key(key="7e647736-57b0-4f08-b324-d9f930945236")
+#' }
+#' @export
+save_key <-function(key){
+  Sys.setenv(HOLIDAYAPI_PAT=key)
+}
+
+#' @title Retrieves the key from users environment
+#' @description Retrieves the key assigned to the variable HOLIDAYAPI_PAT in the users environment.
+#' The key needs to be explicitly saved with the \code{save_key(key)} function.
+#' @return Returns the key from the users environment.
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @examples
+#' \dontrun{get_key()}
+
+get_key <- function(){
+  key <- Sys.getenv('HOLIDAYAPI_PAT', "")
+  if (key == "") {
+    stop("Unable to find the HOLIDAYAPI_PAT in the environment variables. Remember to save the key with the save_key(key) function.")
+  }
+  return(key)
+}
+
+#' @title Retrieve holidays
+#' @description Retrieve a list of holidays from a given country in a given year.
+#' By default the JSON format is selected because it contains additional request information.
+#' @param key The users API key obtained when registering at \href{https://holidayapi.com/signup}{}.
+#' If the key has been saved to the environment before using the \code{save_key(key)} function,
+#' it is not required to pass. It is a required parameter in \code{character} format.
+#' @param country Country code in \href{https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2}{ISO 3166-1 alpha 2}
+#' or \href{https://en.wikipedia.org/wiki/ISO_3166-2}{ISO 3166-2} format. For multiple countries,
+#' pass a vector - it is limited to 10 countries. It is a required parameter in \code{character} format.
+#' @param year Year in \href{https://en.wikipedia.org/wiki/ISO_8601#Years}{ISO 8601} format.
+#' It is a required parameter in \code{numeric} format.
+#' @param ... Ellipsis to pass optional arguments into the function such as:
+#' \itemize{
+#' \item \code{month} - 1 or 2 digit month (1-12). It is an optional parameter.
+#' \item \code{day} - 1 or 2 digit day (1-31 depending on the month). It is an optional parameter format.
+#' \item \code{previous} - 1 or 2 digit day (1-31 depending on the month). It is an optional parameter format.
+#' \item \code{upcoming} - Return upcoming holidays based on the date the API is called. It is an optional parameter format.
+#' \item \code{public} - Return only \href{https://en.wikipedia.org/wiki/Public_holiday}{public holidays}.
+#' It is an optional parameter format in boolean format.
+#' \item \code{language} - By default English language, optionally other languages in ISO 639-1 language
+#' #'  format can be passed (with exceptions). It is an optional parameter format in \code{character} format.
+#' }
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @return Returns a \code{country_holidays} object containing the dates, names of holidays, if they
+#' are public or not. Additionally, relevant information about the API request is shown.
+#' @source For more information about the Holiday API go to the \href{https://holidayapi.com/docs}{Holiday API documentation page}
+#' @examples
+#' \dontrun{
+#' get_holidays(country="ES", year=2018)
+#' }
+#' @section Warning:
+#' Depending on the subscription plan the scope of the API changes. At date 2019-07-01 the free plan
+#' is limited to historical data for 2018 with 10.000 monthly requests in english language format only.
+#'  Non-commercial use only. For more, updated information go \href{https://holidayapi.com/}{here}.
+#' @export
+
+get_holidays<-function(country,
+                       year,
+                       key=get_key(), ...){
+  arguments<-list(...)
+  request_url<-httr::modify_url(url = "https://holidayapi.com/",
+                                path = list(default_version(), "holidays"),
+                                query = list(key=key,
+                                     country=paste0(country, collapse = ","),
+                                     year=year,
+                                     month=arguments$month,
+                                     day=arguments$day,
+                                     previous=arguments$previous,
+                                     upcoming=arguments$upcoming,
+                                     public=arguments$public,
+                                     language=arguments$language))
+  call_raw <- httr::GET(request_url, encode = "json", httr::user_agent("github.com/matbmeijer"))
+  if (httr::http_type(call_raw) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+  call <- jsonlite::fromJSON(httr::content(call_raw, "text"), simplifyVector = TRUE)
+  if(httr::http_error(call_raw)){
+    if(any(names(call) %in% "error")){
+     stop(sprintf("Error Code %s - %s", call$status, call$error))
+    } else {
+      stop(sprintf("Error Code %s - %s", call$status_code, api_response(call$status_code)))
+    }
+  }
+  obj<-as_country_holidays(
+    request_url = request_url,
+    key=key,
+    country=country,
+    year=year,
+    call=call,
+    month=arguments$month,
+    day=arguments$day,
+    previous=arguments$previous,
+    upcoming=arguments$upcoming,
+    public=arguments$public,
+    language=arguments$language,
+    requests_date = as.Date(call_raw$date))
+  return(obj)
+}
+
+#' @title Retrieve available countries
+#' @description Retrieve all 249 for which the Holiday API has holidays available.
+#' Useful to pass on later on \code{to get_holidays()} function. By default the JSON format
+#'  is selected because it contains additional request information.
+#' @param key The users API key obtained when registering at \href{https://holidayapi.com/signup}{}.
+#' If the key has been saved to the environment before using the \code{save_key(key)} function,
+#' it is not required to pass. It is a required parameter in \code{character} format.
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @return Returns a \code{available_countries} object containing the
+#' \href{https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2}{ISO 3166-1 alpha 2} country code and the english short name.
+#' Additionally, relevant information about the API request is shown.
+#' @source For more information about the Holiday API go to the \href{https://holidayapi.com/docs}{Holiday API documentation page}
+#' @examples
+#' \dontrun{
+#' save_key(key="__YOUR_API_KEY__")
+#' get_countries()
+#' }
+#' @section Warning:
+#' Depending on the subscription plan the scope of the API changes. At date 2019-07-01 the
+#' free plan is limited to historical data for 2018 with 10.000 monthly requests in english
+#' language format only. Non-commercial use only. For more, updated information go \href{https://holidayapi.com/}{here}.-
+#' @export
+
+get_countries<-function(key=get_key()){
+  request_url<-httr::modify_url(url = "https://holidayapi.com/",
+                                path = list(default_version(), "countries"),
+                                query = list(key=key))
+  call_raw <- httr::GET(request_url, encode = "json", httr::user_agent("github.com/matbmeijer"))
+  if (httr::http_type(call_raw) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+  call <- jsonlite::fromJSON(httr::content(call_raw, "text"), simplifyVector = TRUE)
+  if(httr::http_error(call_raw)){
+    if(any(names(call) %in% "error")){
+      stop(sprintf("Error Code %s - %s", call$status, call$error))
+    } else {
+      stop(sprintf("Error Code %s - %s", call$status_code, api_response(call$status_code)))
+    }
+  }
+
+  obj<-as_available_countries(request_url = request_url,
+                              key = key,
+                              call = call,
+                              requests_date = as.Date(call_raw$date))
+  return(obj)
+}
+
+#' @title Retrieve available languages
+#' @description Retrieve all 100 for which the Holiday API has holidays translation available.
+#' By default returns english. Useful to pass on later on \code{to get_holidays()} function.
+#' @param key The users API key obtained when registering at \href{https://holidayapi.com/signup}{}.
+#' If the key has been saved to the environment before using the \code{save_key(key)} function, it is not required to pass.
+#'It is a required parameter in \code{character} format.
+#' @author Matthias Brenninkmeijer - \href{https://github.com/matbmeijer}{https://github.com/matbmeijer}
+#' @return Returns a \code{available_languages} object containing the
+#' \href{https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes}{ISO 639-1 format (with exceptions)} language and its name.
+#' Additionally, relevant information about the API request is shown.
+#' @source For more information about the Holiday API go to the \href{https://holidayapi.com/docs}{Holiday API documentation page}
+#' @examples
+#' \dontrun{
+#' save_key(key="__YOUR_API_KEY__")
+#' get_languages()
+#' }
+#' @section Warning:
+#' Depending on the subscription plan the scope of the API changes. At date 2019-07-01 the free plan
+#'  is limited to historical data for 2018 with 10.000 monthly requests in english language format only.
+#'  Non-commercial use only. For more, updated information go \href{https://holidayapi.com/}{here}.-
+#' @export
+
+get_languages<-function(key=get_key()){
+  request_url<-httr::modify_url(url = "https://holidayapi.com/",
+                                path = list(default_version(), "languages"),
+                        query = list(key=key))
+  call_raw <- httr::GET(request_url, encode = "json", httr::user_agent("github.com/matbmeijer"))
+  if (httr::http_type(call_raw) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+  call <- jsonlite::fromJSON(httr::content(call_raw, "text"), simplifyVector = TRUE)
+  if(httr::http_error(call_raw)){
+    if(any(names(call) %in% "error")){
+      stop(sprintf("Error Code %s - %s", call$status, call$error))
+    } else {
+      stop(sprintf("Error Code %s - %s", call$status_code, api_response(call$status_code)))
+    }
+  }
+  obj<-as_available_languages(request_url=request_url,
+                         key = key,
+                         call = call,
+                         requests_date = as.Date(call_raw$date))
+  return(obj)
+}
+
